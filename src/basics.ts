98d3702b93d74393c8e2ae8f36156ba83627d46b
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-01-30 20:06:36 +0100

    Change file format to Prettier VSCode defaults

--- src/basics.ts
+++ src/basics.ts
@@ -1,30 +1,29 @@
-const redux = require("redux");
+const redux = require("redux")
 
-const initialState = { myProperty: 1 };
+const initialState = { myProperty: 1 }
 
 // Reducer
 
 const rootReducer = (currentState = initialState, action) => {
-  if (action.type === "MY_AWESOME_ACTION")
-    return { ...currentState, myProperty: currentState.myProperty + 1 };
-};
+  if (action.type === "MY_AWESOME_ACTION") return { ...currentState, myProperty: currentState.myProperty + 1 }
+}
 
 // Store
 
-const store = redux.createStore(rootReducer);
-console.log("🚀 ~ file: basics.js ~ line 10 ~ store", store.getState());
+const store = redux.createStore(rootReducer)
+console.log("🚀 ~ file: basics.js ~ line 10 ~ store", store.getState())
 
 // Subscription
 
 store.subscribe(() => {
-  console.log("🚀 ~ file: basics.js ~ line 15 ~ store", store.getState());
-});
+  console.log("🚀 ~ file: basics.js ~ line 15 ~ store", store.getState())
+})
 
 // Action
 
-const myAction = { type: "MY_AWESOME_ACTION" };
+const myAction = { type: "MY_AWESOME_ACTION" }
 
 // Dispatch
 
-store.dispatch(myAction);
-console.log("🚀 ~ file: basics.js ~ line 30 ~ store", store.getState());
+store.dispatch(myAction)
+console.log("🚀 ~ file: basics.js ~ line 30 ~ store", store.getState())
